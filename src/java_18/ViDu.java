1cdd02dce385eae43b5fc14f798d0d52886469d3
chipchippp <locnvth2209036@fpt.edu.vn>
2023-10-16 21:19:06 +0700

    java

---
+++ src/java_18/ViDu.java
@@ -0,0 +1,16 @@
+package java_18;
+
+import java.util.Scanner;
+
+public class ViDu {
+    public static void main(String[] args) {
+        double a, b, c, x1,x2, delta;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập a = ");
+        a = sc.nextDouble();
+        System.out.println("nhập b = ");
+        b = sc.nextDouble();
+        System.out.println("nhập c = ");
+        c = sc.nextDouble();
+    }
+}
