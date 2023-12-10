af3e2cab8b64e5eb6c343b39e426c8b23252f36f
chipchippp <locnvth2209036@fpt.edu.vn>
2023-12-10 23:42:45 +0700

    super 2

--- src/inheritance/EnterpriseUser.java
+++ src/inheritance/EnterpriseUser.java
@@ -7,10 +7,10 @@ public class EnterpriseUser extends User{
     public EnterpriseUser(){
         System.out.println("Enterprise User");
     }
-//    public EnterpriseUser(int parentTemp, int myTemp){
-//        super.temporaryValue = parentTemp;
-//        this.temporaryValue = myTemp;
-//    }
+    public EnterpriseUser(int parentTemp, int myTemp){
+        super.temporaryValue = parentTemp;
+        this.temporaryValue = myTemp;
+    }
 
     public EnterpriseUser(String name, String email, String password, String company) {
         super(name, email, password);
@@ -20,5 +20,10 @@ public class EnterpriseUser extends User{
     public void printEnterpriseUserInfo(){
         super.printInfo();
         System.out.println("company " + this.company);
+    }
+    public void printTemporaryValues(){
+        System.out.println("ParenTemp " + this.temporaryValue);
+        System.out.println("My Temp " + this.temporaryValue);
+
     }
 }
--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -2,9 +2,12 @@ package inheritance;
 
 public class Main {
     public static void main(String[] args) {
-       UniversityUser amy = new UniversityUser();
+//       UniversityUser amy = new UniversityUser();
 
-       EnterpriseUser steve = new EnterpriseUser("steve","steve@gmail.com","1234","Java");
-       steve.printEnterpriseUserInfo();
+//       EnterpriseUser steve = new EnterpriseUser("steve","steve@gmail.com","1234","Java");
+//       steve.printEnterpriseUserInfo();
+
+        EnterpriseUser grace = new EnterpriseUser(200, 100);
+        grace.printTemporaryValues();
     }
 }
