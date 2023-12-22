6be7e994f6f4b950e7d41ecca9147287769bbc8d
chipchippp <locnvth2209036@fpt.edu.vn>
2023-12-23 00:16:12 +0700

    tinh da hinh 2 c2 cos đa hiình

---
+++ src/daHinh/LoginManagerImproved.java
@@ -0,0 +1,10 @@
+package daHinh;
+
+public class LoginManagerImproved {
+    public void processLogin(User user){
+        System.out.println("process something before login");
+        user.login();
+        user.enterPage();
+        System.out.println("process something after login \n");
+    }
+}
--- src/daHinh/Main.java
+++ src/daHinh/Main.java
@@ -4,16 +4,17 @@ package daHinh;
 
 public class Main {
     public static void main(String[] args) {
-        LoginManager loginManager = new LoginManager();
+        LoginManagerImproved loginManager = new LoginManagerImproved();
 
-        PaidUser paid = new PaidUser("Paid","paid@gmail.com","1111","Pro");
-        loginManager.processLogin(paid);
 
-        EnterpriseUser enterprise = new EnterpriseUser("Enterprise","enterprise@gmail.com","2222","Vip");
-        loginManager.processLogin(enterprise);
+        User user = new PaidUser("Paid","paid@gmail.com","1111","Pro");
+        loginManager.processLogin(user);
 
-        Administrator admin = new Administrator("Admin","admin@gmail.com","3333","Svip");
-        loginManager.processLogin(admin);
+        user = new EnterpriseUser("Enterprise","enterprise@gmail.com","2222","Vip");
+        loginManager.processLogin(user);
+
+        user = new Administrator("Admin","admin@gmail.com","3333","Svip");
+        loginManager.processLogin(user);
 
 
 
