01428192807ab927ed72019250dbdf8758ad4e4b
chipchippp <locnvth2209036@fpt.edu.vn>
2023-12-24 23:54:17 +0700

    java 33

---
+++ src/java_33/Main.java
@@ -0,0 +1,11 @@
+package java_33;
+
+public class Main {
+    public static void main(String[] args) {
+        MyDate md1 = new MyDate(14,5,2021);
+        MyDate md2 = new MyDate(12,3,2023);
+        MyDate md3 = new MyDate(12,5,2312);
+
+        System.out.println(md1);
+    }
+}
