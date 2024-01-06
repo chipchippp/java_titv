78c8201594085e51601d5840027aa2d1803ff6d7
chipchippp <locnvth2209036@fpt.edu.vn>
2024-01-06 15:18:54 +0700

    java 57

---
+++ src/java_57/Main.java
@@ -0,0 +1,48 @@
+package java_57;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++){
+            System.out.println("TEST");
+        }
+        long t2 = System.currentTimeMillis();
+
+        System.out.println("Trước khi chạy for: "+t1);
+        System.out.println("Sau khi chạy for: "+t2);
+        System.out.println("Thời gian chạy: "+((t2-t1)) + "mls");
+        System.out.println("Thời gian chạy: "+((t2-t1)/1000) + "s");
+
+        //timeUnit
+        System.out.println("3000 năm = "+ TimeUnit.DAYS.toSeconds(3000*365)+"s");
+
+        System.out.println("24h = "+ TimeUnit.HOURS.toSeconds(24)+"s");
+
+        // Date
+        Date d = new Date(System.currentTimeMillis());
+        System.out.println(d.getDate()+"/"+(d.getMonth()+1)+"/"+(d.getYear()+1900));
+
+        //Calendar
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.get(Calendar.DATE)+"-"+(c.get(Calendar.MONTH)+1)+"-"+c.get(Calendar.YEAR));
+
+        c.add(Calendar.HOUR, 30);
+        System.out.println(c.get(Calendar.DATE)+"-"+(c.get(Calendar.MONTH)+1)+"-"+c.get(Calendar.YEAR));
+
+        c.add(Calendar.DATE, 30);
+        System.out.println(c.get(Calendar.DATE)+"-"+(c.get(Calendar.MONTH)+1)+"-"+c.get(Calendar.YEAR));
+
+        //DateFormat
+        DateFormat df = new SimpleDateFormat();
+        System.out.println(df.format(d));
+
+        df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(df.format(d));
+    }
+}
